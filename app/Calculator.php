04f26711f2e0d52c5db2ada80202d6f50a1a78a9
nahumsin <nahumaldonado@gmail.com>
2014-11-10 09:04:46 -0600

    Agregando directorios

---
+++ app/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+	class Calculator{
+		public function suma($num1,$num2){
+			return $num1+$num2;
+		}
+		public function resta($num1,$num2){
+			return $num1-$num2;
+		}
+		public function multiplicar($num1,$num2){
+			return $num1*$num2;
+		}
+		public function dividir($num1,$num2){
+			return $num1/$num2;
+		}
+	}
+?>
